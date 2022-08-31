2c1bc2a664dfa0cfe500974b699515bdd8f26566
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-08-31 20:42:51 +0200

    202220831

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HiArrowHarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div className=' w-full h-screen bg-[#08192f] '>
+
+    </div>
+  )
+}
+
+export default Home     

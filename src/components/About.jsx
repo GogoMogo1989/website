5beabdbab273111a471a3cb0aff031f7d7f307b4
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-09-02 10:53:40 +0200

    20220902

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-[#08192f] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className="sm:text-right pb-8 pl-4">
+                    <p className='text-4xl font-bold inline border-b-4 border-[#7f779b]'>About</p>
+                </div>
+                <div></div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div>
+                    <p className='sm:text-right text-4xl font-bold '>Hi! I'm David, I'm glad to here you. Please look around.</p>
+                </div>
+                <div>
+                    <p>I always intrested in IT, thats why I decided, I learn a new proffesion. I have already learning in highschool to html, css, and javascript, and I realy liked it, thats why I choice to frontend developer proffesion. I made many webpage, and I would like to show it to you! I hope you like it!:-) If you would like to contact me, please write me a email! You have find it my email in Contact section.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import {HiArrowHarrowRight} from 'react-icons/hi'
+import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className=' w-full h-screen bg-[#08192f] '>
+    <div name="home" className=' w-full h-screen bg-[#08192f] '>
+
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className='text-[#7f779b]'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font:bold text-[#ccc6f6]'>Kovács-Farkas Dávid</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>Welcome to my webpage! You can see here my projects, and also you can found my availability.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#7f779b] hover:border-[#7f779b]">View Work
+          <span className='group-hover:rotate-90 duration-300'>
+          <HiArrowNarrowRight className='ml-2'/>
+          </span> 
+          </button>
+        </div>
+      </div>
 
     </div>
   )

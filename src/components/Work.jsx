99dd29304e92b8415b6280d2933f8979a1e1f3bc
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-09-03 20:38:53 +0200

    20220903

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -27,6 +27,21 @@ const Work = () => {
                                 </a>
                             </div>
                         </div>
+
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Javascript NASA
+                            </span>
+                            <div className='pt-8 text-center '>
+                                <a href="https://gogomogo1989.github.io/nasa/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href="https://github.com/GogoMogo1989/nasa">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+
                 </div>
             </div>
         </div>

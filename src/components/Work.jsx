c1f7d493359c460d0b5c9cd3a953d97286fae2b5
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-09-03 21:57:05 +0200

    20220903

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/Weather-Api.jpg'
 import WorkImg2 from '../assets/Nasa.jpg'
+import WorkImg3 from '../assets/Profil.jpg'
 
 const Work = () => {
   return (
@@ -29,6 +30,7 @@ const Work = () => {
                             </div>
                         </div>
                 </div>
+                
                 <div style={{backgroundImage: `url(${WorkImg2})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         <div className='opacity-0 group-hover:opacity-100'>
@@ -44,8 +46,23 @@ const Work = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
                 </div>
+
+                <div style={{backgroundImage: `url(${WorkImg3})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Javascript NASA
+                            </span>
+                            <div className='pt-8 text-center '>
+                                <a href="https://github.com/GogoMogo1989/profile">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                </div>
+
+            </div>
         </div>
     </div>
   )

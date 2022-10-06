08c7dd08c0a62ec5c0b6ba99bb7b74b59439bb79
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-06 11:06:41 +0200

    20221006

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../assets/wolflogo.png'
+import Logo from '../assets/wolf.png'
 import {Link} from 'react-scroll'
 
 const Navbar = () => {

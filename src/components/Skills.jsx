e14e08be255a27390c10006dbec4a05414b5a3d4
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-18 16:41:14 +0200

    20221018

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,7 +22,7 @@ export const Skills = () => {
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className='w-20 mx-auto mt-10' src={HTML} alt="html"/>
+            <img className='w-20 mx-auto mt-2.5' src={HTML} alt="html"/>
             <p className='my-4'>HTML</p>
           </div>
   

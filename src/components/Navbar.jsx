a56a99ed2ce70a486cdad8e6914d01396478699b
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-11-13 13:22:45 +0100

    fixxed issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-400 z-10'>
-      {/* Main menu */}
+    
       <ul className="hidden md:flex">
         <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
         <li><Link to="about" smooth={true} duration={500}>About</Link></li>
@@ -21,12 +21,10 @@ const Navbar = () => {
         <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
 
-      {/* Hamburger menu */}
       <div onClick={handleClick} className="md:hidden z-10">
         {nav ? <FaTimes /> : <FaBars />}
       </div>
 
-      {/* Mobile menu */}
       <ul className={nav ? 'absolute top-0 left-0 w-full h-screen bg-[rgb(11,9,9)] flex flex-col justify-center items-center text-gray-400' : 'hidden'}>
         <li className="py-6 text-4xl"><Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link></li>
         <li className="py-6 text-4xl"><Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link></li>
@@ -38,26 +36,30 @@ const Navbar = () => {
 
       <div className='fixed top-[35%] left-[50px] flex flex-col'>
         <ul>
-          <li className='w-[60px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-50px] duration-300 bg-blue-600'>
+          <li className='w-[80px] h-[60px] flex justify-between items-center ml-[-120px] hover:ml-[-50px] duration-300 bg-blue-600'>
             <a className='flex justify-center items-center w-full text-white' href="https://www.linkedin.com/in/d%C3%A1vid-kov%C3%A1cs-farkas-733732241/">
               <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[60px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-50px] duration-300 bg-[#333333]'>
+          <li className='w-[80px] h-[60px] flex justify-between items-center ml-[-120px] hover:ml-[-50px] duration-300 bg-[#333333]'>
             <a className='flex justify-center items-center w-full text-white' href="https://github.com/GogoMogo1989">
              <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[60px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-50px] duration-300 bg-[#6fc2b0]'>
-            <a className='flex justify-center items-center w-full text-white' href="/">
-              <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className='w-[60px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-50px] duration-300 bg-[#565f69]'>
-            <a className='flex justify-center items-center w-full text-white' href="/">
-              <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
+          <Link to="email" smooth={true} duration={500} >
+            <li className='w-[80px] h-[60px] flex justify-between items-center ml-[-120px] hover:ml-[-50px] duration-300 bg-[#6fc2b0]'>
+                <a className='flex justify-center items-center w-full text-white' href="/">
+                <HiOutlineMail size={30} />
+                </a>
+            </li>
+          </Link>
+          <Link to="resumee" smooth={true} duration={500}>
+            <li className='w-[80px] h-[60px] flex justify-between items-center ml-[-120px] hover:ml-[-50px] duration-300 bg-[#565f69]'>
+                <a className='flex justify-center items-center w-full text-white' href="/">
+                <BsFillPersonLinesFill size={30} />
+                </a>
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
